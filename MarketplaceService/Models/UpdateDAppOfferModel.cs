51696bda44600a28198465d44add19df61c70266
Joeri Berman <joeriberman@hotmail.com>
2020-05-29 10:35:00 +0200

    S65-177: Added even more Cors configs to startup

--- MarketplaceService/Models/UpdateDAppOfferModel.cs
+++ MarketplaceService/Models/UpdateDAppOfferModel.cs
@@ -10,7 +10,6 @@ namespace MarketplaceService.Models
         [Required] public int OfferLengthInMonths { get; set; }
         [Required] public int LiskPerMonth { get; set; } 
         [Required] public int DelegatesNeededForOffer { get; set; }
-
         [Required] public string Region { get; set; }
         [Required] public DateTime DateStart { get; set; }
         [Required] public DateTime DateEnd { get; set; }
--- MarketplaceService/Startup.cs
+++ MarketplaceService/Startup.cs
@@ -44,6 +44,11 @@ namespace MarketplaceService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+            );
             
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
